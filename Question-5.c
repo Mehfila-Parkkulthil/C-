7cd88db0418fe730207f7dd3d32df86886fdcfcd
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-10 14:28:12 +0530

    added files

---
+++ Question-5.c
@@ -0,0 +1,10 @@
+//Question 5
+//Create a program to ask age and print their age
+#include <stdio.h>
+int main(){
+    int age;
+    printf("Hi \n");
+    printf("Pls enter your age: \n");
+    scanf("%d", &age);
+    printf("So your  %d years old . ", age);
+}
---
+++ Question-6.c
@@ -0,0 +1,14 @@
+// Question 6
+// Create a program to calculate the area and perimeter of square
+
+#include <stdio.h>
+int main(){
+    int side;
+    printf("Lets calculate the area and perimeter of square \n");
+    printf("Pls enter your side:");
+    scanf("%d",&side);
+    printf("So area of your square is %d \n" , side*side);
+    printf("And perimeter of your square is %d \n", 4*side);
+    return 0;
+
+}
