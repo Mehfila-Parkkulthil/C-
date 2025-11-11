7f6486b0d071d12d0b3f715e34ee7c29de9c8317
Mehfila123 <mehfilaparkkulthil@gmail.com>
2025-11-11 15:14:17 +0530

    added readme.md

---
+++ Question-38.c
@@ -0,0 +1,11 @@
+//Question 38
+//Create a program to print from 1000 -1 using for loop 
+
+#include <stdio.h>
+int main(){
+    printf("Lets print from 1000 - 1 using for loop \n");
+    for(int i =1000 ; i>=1;i--){
+        printf("%d\n",i);
+    }
+    return 0;
+}
